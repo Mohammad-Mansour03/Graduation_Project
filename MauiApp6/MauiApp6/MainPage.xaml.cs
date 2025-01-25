0747ceebd3891319b42a61e3f3ced64c74fcf8b2
fares haroun <far0213025@ju.edu.jo>
2025-01-25 15:45:15 +0300

    maui project init

---
+++ MauiApp6/MauiApp6/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp6
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
