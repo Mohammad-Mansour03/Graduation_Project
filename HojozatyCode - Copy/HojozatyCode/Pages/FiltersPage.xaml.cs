b6f9069c4795d2967b19bc34a7f83cf48b9883c9
Raad Alkhatib <alkhatib0017@gmail.com>
2025-03-18 18:38:43 +0300

    updated filters and added a slider

--- HojozatyCode - Copy/HojozatyCode/Pages/FiltersPage.xaml.cs
+++ HojozatyCode - Copy/HojozatyCode/Pages/FiltersPage.xaml.cs
@@ -9,5 +9,12 @@ namespace HojozatyCode.Pages
             InitializeComponent();
             BindingContext = new LogInViewModel();
         }
+
+        // Event handler for when the slider value changes
+        private void PriceSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            // Update the label to display the slider value, converting it to an integer and appending " JD"
+            PriceValueLabel.Text = $"{(int)e.NewValue} JD";
+        }
     }
 }
