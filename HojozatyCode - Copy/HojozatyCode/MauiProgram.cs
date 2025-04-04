ef0e862e229c97bdca1f66e256d9b1d76596bc38
Raad Alkhatib <alkhatib0017@gmail.com>
2025-04-04 23:13:31 +0300

    finalize category

--- HojozatyCode - Copy/HojozatyCode/MauiProgram.cs
+++ HojozatyCode - Copy/HojozatyCode/MauiProgram.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.Extensions.Logging;
-using HojozatyCode.ViewModels; // Add this line if CategoryVenuesViewModel is in the ViewModels namespace
+using HojozatyCode.ViewModels;
 using CommunityToolkit.Maui;
+
+#if ANDROID
+using Android.Graphics.Drawables;
+#endif
+
 namespace HojozatyCode
 {
     public static class MauiProgram
@@ -9,29 +14,37 @@ namespace HojozatyCode
         {
             var builder = MauiApp.CreateBuilder();
 
-            builder.UseMauiApp<App>().UseMauiCommunityToolkit();
-
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit();
 
 #if DEBUG
-			builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
-        
-        builder.Services.AddTransient<AddSpaceViewModel>();
-        builder.Services.AddTransient<Pages.SpaceTypeSelectionPage>();
-        builder.Services.AddTransient<Pages.SpaceInformationPage>();
-        builder.Services.AddTransient<Pages.CategoryVenuesPage>();
-        builder.Services.AddTransient<CategoryVenuesViewModel>();
-        builder.Services.AddTransient<Pages.ServicesPage>();
-        builder.Services.AddTransient<Pages.SpacePicturesPage>();
-        builder.Services.AddTransient<Pages.EditProfile>();
-        builder.Services.AddSingleton<AddSpaceViewModel>(); // for managing the state of the AddSpaceViewModel across the app lifecycle 
-        builder.Services.AddTransient<Pages.ExplorePage>();
-        builder.Services.AddTransient<Pages.AdminApprovalPage>();
-        builder.Services.AddTransient<AdminApprovalViewModel>();
-        builder.Services.AddSingleton<AdminApprovalViewModel>(); // for managing the state of the AdminApprovalViewModel across the app lifecycle
-        
-        
+            // Register ViewModels and Pages
+            builder.Services.AddTransient<AddSpaceViewModel>();
+            builder.Services.AddTransient<Pages.SpaceTypeSelectionPage>();
+            builder.Services.AddTransient<Pages.SpaceInformationPage>();
+            builder.Services.AddTransient<Pages.CategoryVenuesPage>();
+            builder.Services.AddTransient<CategoryVenuesViewModel>();
+            builder.Services.AddTransient<Pages.ServicesPage>();
+            builder.Services.AddTransient<Pages.SpacePicturesPage>();
+            builder.Services.AddTransient<Pages.EditProfile>();
+            builder.Services.AddSingleton<AddSpaceViewModel>();
+            builder.Services.AddTransient<Pages.ExplorePage>();
+            builder.Services.AddTransient<Pages.AdminApprovalPage>();
+            builder.Services.AddTransient<AdminApprovalViewModel>();
+            builder.Services.AddSingleton<AdminApprovalViewModel>();
+
+#if ANDROID
+            // Remove underline from Picker on Android
+            Microsoft.Maui.Handlers.PickerHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+            {
+                handler.PlatformView.Background = new ColorDrawable(Android.Graphics.Color.Transparent);
+            });
+#endif
+
             return builder.Build();
         }
     }
