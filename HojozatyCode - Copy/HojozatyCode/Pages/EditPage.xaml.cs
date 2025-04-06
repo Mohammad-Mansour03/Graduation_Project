62185aa94c71bf4597c3f6924555855a5d78e138
Mohammad-Mansour03 <mohammadalejil03@gmail.com>
2025-04-06 23:36:18 +0300

    Solve the Add Space Problem

--- HojozatyCode - Copy/HojozatyCode/Pages/EditPage.xaml.cs
+++ HojozatyCode - Copy/HojozatyCode/Pages/EditPage.xaml.cs
@@ -9,24 +9,8 @@ public partial class EditPage: ContentPage
     {
         InitializeComponent();
         _viewModel = (MySpaceViewModel)BindingContext;
-    }
 
-<<<<<<< HEAD
-    //protected override async void OnAppearing()
-    //{
-    //    base.OnAppearing();
-
-    //    // Optional: load any needed initial data
-    //    await _viewModel.LoadInitialDataAsync();
-    //}
-}
-=======
-    protected override async void OnAppearing()
-    {
-        base.OnAppearing();
-        await _viewModel.LoadVenuesAsync();
     }
 
 
 }
->>>>>>> 4ea658f25d12c24cbdd2a7f9b3fbec2f163a8c8e
