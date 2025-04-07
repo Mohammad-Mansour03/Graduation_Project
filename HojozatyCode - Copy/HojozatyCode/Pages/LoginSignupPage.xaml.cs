3a7493d3eaa69279ed5ba32326d67f866ddaf3be
Raad Alkhatib <alkhatib0017@gmail.com>
2025-04-07 23:28:17 +0300

    added animations to login/signup page and polished it and made it look nicer
    :)

--- HojozatyCode - Copy/HojozatyCode/Pages/LoginSignupPage.xaml.cs
+++ HojozatyCode - Copy/HojozatyCode/Pages/LoginSignupPage.xaml.cs
@@ -1,4 +1,5 @@
 using HojozatyCode.ViewModels;
+using Microsoft.Maui.Controls;
 
 namespace HojozatyCode.Pages
 {
@@ -8,6 +9,22 @@ namespace HojozatyCode.Pages
         {
             InitializeComponent();
             BindingContext = new LoginSignupViewModel();
+            AnimatePage();
+        }
+
+        private async void AnimatePage()
+        {
+            // Logo animation
+            await LogoImage.FadeTo(1, 700, Easing.CubicOut);
+            await LogoImage.ScaleTo(1, 400, Easing.CubicOut);
+
+            // Subtitle animation
+            await SubtitleLabel.FadeTo(1, 600, Easing.CubicIn);
+
+            // Buttons animation
+            await Task.Delay(200);
+            await ButtonStack.FadeTo(1, 600, Easing.CubicIn);
+            await ButtonStack.ScaleTo(1, 300, Easing.SpringOut);
         }
     }
 }
