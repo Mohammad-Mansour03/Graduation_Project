dd5a035a09b2497e4e26e675a052665aa26516ca
Raad Alkhatib <alkhatib0017@gmail.com>
2025-04-09 23:18:08 +0300

    update

--- HojozatyCode - Copy/HojozatyCode/Pages/LogInPage.xaml.cs
+++ HojozatyCode - Copy/HojozatyCode/Pages/LogInPage.xaml.cs
@@ -9,5 +9,18 @@ namespace HojozatyCode.Pages
             InitializeComponent();
             BindingContext = new LogInViewModel();
         }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Fade-in animation
+            this.Opacity = 0;
+            await this.FadeTo(1, 800, Easing.CubicInOut);
+
+            // Slide in Back Button
+            BackButton.TranslationX = -100;
+            await BackButton.TranslateTo(0, 0, 500, Easing.CubicOut);
+        }
     }
 }
