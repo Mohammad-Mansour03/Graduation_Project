ad5c9455d18b4b917ae336e4d3182b0470709764
fares haroun <far0213025@ju.edu.jo>
2025-04-29 21:19:36 +0300

    Refactor AddSpaceViewModel to improve form clearing and reset functionality; implement ResetViewState method in SpaceTypeSelectionPage

--- HojozatyCode - Copy/HojozatyCode/Pages/SpaceTypeSelectionPage.xaml.cs
+++ HojozatyCode - Copy/HojozatyCode/Pages/SpaceTypeSelectionPage.xaml.cs
@@ -4,9 +4,18 @@ namespace HojozatyCode.Pages;
 
 public partial class SpaceTypeSelectionPage : ContentPage
 {
+    private readonly AddSpaceViewModel _viewModel;
+
     public SpaceTypeSelectionPage(AddSpaceViewModel viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
+        _viewModel = viewModel;
+    }
+    
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _viewModel.ResetViewState();
     }
 }
