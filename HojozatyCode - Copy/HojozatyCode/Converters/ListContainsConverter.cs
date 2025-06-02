6b5f52b3f0b48af2b40472a04a2f0a438d46d865
Raad Alkhatib <alkhatib0017@gmail.com>
2025-06-02 15:02:12 +0300

    fix apk crash

--- HojozatyCode - Copy/HojozatyCode/Converters/ListContainsConverter.cs
+++ HojozatyCode - Copy/HojozatyCode/Converters/ListContainsConverter.cs
@@ -1,17 +1,22 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Globalization;
+using Microsoft.Maui.Controls; // Make sure you're using this for .NET MAUI
 
-public class ListContainsConverter : IValueConverter
+namespace HojozatyCode.Converters // <- Add this
 {
-	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-	{
-		var list = value as ObservableCollection<string>;
-		var item = parameter as string;
-		return list != null && list.Contains(item);
-	}
+    public class ListContainsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var list = value as ObservableCollection<string>;
+            var item = parameter as string;
+            return list != null && list.Contains(item);
+        }
 
-	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-	{
-		throw new NotImplementedException(); // Not used
-	}
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException(); // Not used
+        }
+    }
 }
--- HojozatyCode - Copy/HojozatyCode/Pages/FiltersPage.xaml.cs
+++ HojozatyCode - Copy/HojozatyCode/Pages/FiltersPage.xaml.cs
@@ -1,4 +1,5 @@
 using HojozatyCode.ViewModels;
+using Microsoft.Maui.Controls;
 
 namespace HojozatyCode.Pages
 {
@@ -7,24 +8,26 @@ namespace HojozatyCode.Pages
         public FiltersPage()
         {
             InitializeComponent();
-            BindingContext = new FilterVenueViewModel();
+            BindingContext = new FilterVenueViewModel(); // Use your real ViewModel
         }
-		private void OnCategoryCheckedChanged(object sender, CheckedChangedEventArgs e)
-		{
-			if (BindingContext is FilterVenueViewModel viewModel && sender is CheckBox checkBox && checkBox.BindingContext is string category)
-			{
-				if (e.Value)
-				{
-					if (!viewModel.SelectedCategories.Contains(category))
-						viewModel.SelectedCategories.Add(category);
-				}
-				else
-				{
-					if (viewModel.SelectedCategories.Contains(category))
-						viewModel.SelectedCategories.Remove(category);
-				}
-			}
-		}
 
-	}
+        private void OnCategoryCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (BindingContext is FilterVenueViewModel vm &&
+                sender is CheckBox checkBox &&
+                checkBox.BindingContext is string category)
+            {
+                if (e.Value)
+                {
+                    if (!vm.SelectedCategories.Contains(category))
+                        vm.SelectedCategories.Add(category);
+                }
+                else
+                {
+                    if (vm.SelectedCategories.Contains(category))
+                        vm.SelectedCategories.Remove(category);
+                }
+            }
+        }
+    }
 }
